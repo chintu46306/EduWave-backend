6278135ef18c8e6aad7c736f520de933fcff5e16
chintu46306 <crg46306@gmail.com>
2024-02-09 22:46:18 +0530

    added: login in userController

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,5 @@
-import User from "../models/user.model";
-import AppError from "../utils/error.util";
+import User from "../models/user.model.js";
+import AppError from "../utils/error.util.js";
 
 const cookieOptions = {
     maxAge: 7 * 24 * 60 * 60 * 1000,  // 7 days
@@ -14,14 +14,15 @@ const register = async (req, res, next) => {
         return next(new AppError('Please enter all fields', 400));
     }
 
+    console.log();
     const userExists = await User.findOne({ email });
-
-    if (!userExists) {
+    console.log(userExists);
+    if (userExists) {
         return next(new AppError('Email already exists', 400));
     }
 
     const user = await User.create({
-        fulName,
+        fullName,
         email,
         password,
         avatar: {
@@ -52,7 +53,37 @@ const register = async (req, res, next) => {
 
 };
 
-const login = (req, res) => {
+const login = async (req, res) => {
+
+    try{
+        const { email, password } = req.body;
+    
+        if (!email || !password) {
+            return next(new AppError('Please enter all fields', 400));
+        }
+    
+        const user = await User.findOne({ 
+            email 
+        }).select('+password');  
+    
+        if (!user || !user.comparePassword(password)){
+            return next(new AppError('Email or password does not match', 401));
+        }
+    
+        const token = await user.generateJWTToken();
+        user.password = undefined;
+    
+        res.cookie('token', token, cookieOptions);
+    
+        res.status(200).json({
+            success: true,
+            message: 'User logged in successfully',
+            user,
+        });
+    }catch(e){
+        return next(new AppError(e.message, 500));
+    }
+
 
 };
 
