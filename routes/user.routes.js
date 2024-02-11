7db84ecaed70ad8948f08016720bd2523d302c60
chintu46306 <crg46306@gmail.com>
2024-02-11 13:23:52 +0530

    added : import isLoggedIn and upload

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,12 @@
 import {Router} from "express";  // Require express.Router() from express module 
 import {register, login, logout, getProfile} from '../controllers/user.controller.js';  // Require register, login, logout, getProfile from user.controller.js
-
+import { isLoggedIn } from "../middlewares/auth.middleware.js";
+import upload from "../middlewares/multer.middleware.js";
 
 const router = Router();  // Create router
 
 
-router.post('/register', register);
+router.post('/register', upload.single("avatar"), register);
 router.post('/login', login);
 router.get('/logout', logout);
 router.get('/me', isLoggedIn, getProfile);
