ede4dfe89d82e35b36872b0ebee306ea799f96f3
chintu46306 <crg46306@gmail.com>
2024-03-03 21:04:00 +0530

    created Course Router

---
+++ routes/course.routes.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { getAllCourses, getLecturesByCourseId } from '../controllers/course.controller.js';
+import { isLoggedIn } from '../middlewares/auth.middleware.js';
+
+const router = Router();
+
+router.route('/',)
+    .get(getAllCourses);
+
+router.route('/:id')
+    .get(isLoggedIn, getLecturesByCourseId);
+
+export default router;
